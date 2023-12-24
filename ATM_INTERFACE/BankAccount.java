6890f80b24af5b5a468732a02c101c35749d30be
Priyanshu Dey <142584429+deypriyanshu@users.noreply.github.com>
2023-12-24 23:24:17 +0530

    Create BankAccount.java

---
+++ ATM_INTERFACE/BankAccount.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: $" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawal successful. New balance: $" + balance);
+            return true;
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance.");
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount userAccount = new BankAccount(1000.0); // Initial balance of $1000
+        ATM atm = new ATM(userAccount);
+        atm.run();
+    }
+}
